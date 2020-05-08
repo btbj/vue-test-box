4eac7fa569cfd40d4202e10e39d61fcd3a929147
btbj <hbjbtbj@gmail.com>
2020-05-08 11:21:11 +0800

    update export file

--- packages/index.js
+++ packages/index.js
@@ -1,25 +1,16 @@
-export { default as VueTestBox } from './VueTestBox/index.vue'
-// import VueTestBox from './VueTestBox'
-// // 存储组件列表
-// const components = [
-//   VueTestBox
-// ]
-// /*
-//   定义install 方法，接收Vue作为参数，如果使用use注册插件，则所有的组件都将被注册
-// */
-// const install = function (Vue) {
-//   // 判断是否安装
-//   if (install.installed) return
-//   // 遍历所有组件
-//   components.map(component => {
-//     Vue.component(component.name, component)
-//   })
-// }
-// // 判断是否引入文件
-// if (typeof window !== 'undefined' && window.Vue) {
-//   install(window.Vue)
-// }
-// export default {
-//   install,
-//   VueTestBox
-// }
+import VueTestBox from './VueTestBox'
+/*
+  定义install 方法，接收Vue作为参数，如果使用use注册插件，则所有的组件都将被注册
+*/
+const install = function (Vue) {
+  Vue.component(VueTestBox.name, VueTestBox)
+}
+// 判断是否引入文件
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+export default {
+  VueTestBox,
+  install
+}
+export { VueTestBox, install }
